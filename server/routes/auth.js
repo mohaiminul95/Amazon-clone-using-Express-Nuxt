2a6b6bbe2a1c97194c459df2028bcee87311b785
mohaiminul94 <rabidislam94@gmail.com>
2021-05-17 17:10:55 +0600

    jwt updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const verifyToken = require('../middlewares/verify-token');
 const jwt = require('jsonwebtoken');
 
 /* Signup route */
@@ -33,5 +34,59 @@ router.post('/auth/signup', async (req, res) => {
         }
     }
 });
+
+/* user Profile */
+router.get('/auth/user', verifyToken, async (req, res) => {
+    try{
+        // let foundUser = await User.findOne({ _id: req.decoded._id }).populate("address").exec();
+        let foundUser = await User.findOne({ _id: req.decoded._id });
+        if(foundUser) {
+            res.json({
+                success: true,
+                user: foundUser
+            })
+        }
+    } catch (e) {
+        res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
+})
+
+/* Login */
+router.post("/auth/login", async (req, res) => {
+    try{
+        let foundUser = await User.findOne({ email: req.body.email });
+        if(!foundUser)
+        {
+            res.status(403).json({
+                success: false,
+                message: "Authentication failed, User not found!!!"
+            })
+        } else {
+            if(foundUser.comparePassword(req.body.password)) {
+                let token = jwt.sign(foundUser.toJSON(), process.env.SECRET, {
+                    expiresIn: 604800   // 1week
+                });
+                res.json({
+                    success: true,
+                    token: token,
+                    message: "Login Successful"
+                });
+            } else {
+                res.json({
+                    success: false,
+                    message: "Authentication failed, Invalid password!!"
+                })
+            }
+        }
+    } catch (e) {
+        res.status(500).json({
+            success: false,
+            message: e.message
+        })
+    }
+})
 
 module.exports = router;
